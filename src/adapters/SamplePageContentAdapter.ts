21d27d2460204b8a43f7c23a9ff6d807aec81978
ipselon <apustovalov@gmail.com>
2023-05-22 13:03:47 +0300

    Update

--- src/adapters/SamplePageContentAdapter.ts
+++ src/adapters/SamplePageContentAdapter.ts
@@ -10,8 +10,8 @@ export class SamplePageContentAdapter extends ContentAdapter<SamplePageContent> 
             dateUpdated: content?.dateUpdated,
             authors: content?.authors,
             path: path || '',
-            locale,
-            hasRestrictedAreas,
+            locale: locale || '',
+            hasRestrictedAreas: !!hasRestrictedAreas,
             baseUrl: baseUrl || '',
             documentAreas: {
                 metaData: [],
